a4fbc7dc62640dc59d52b88ebe5a83bc81884710
kridsanapakron-m <dgsx.64854@gmail.com>
2025-10-24 15:03:44 +0700

    Modify main file to process required commands

--- ipa2024_final.py
+++ ipa2024_final.py
@@ -10,7 +10,8 @@ import json
 import time
 import glob
 import requests
-from restconf_final import create, enable, disable, delete, status
+import restconf_final
+import netconf_final
 from netmiko_final import gigabit_status
 from ansible_final import showrun
 from fine_iparoom import get_room_id
@@ -18,6 +19,19 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 from requests_toolbelt.multipart.encoder import MultipartEncoder
+command_type = 0
+router_ip = None
+def check_type_command(message):
+    global command_type
+    if message == "restconf":
+        command_type = 1
+        return "Ok: Restconf"
+    elif message == "netconf":
+        command_type = 2
+        return "Ok: Netconf"
+    else:        
+        command_type = 0
+        return "Error: No method specified"
 #######################################################################################
 # 2. Assign the Webex access token to the variable ACCESS_TOKEN using environment variables.
 
@@ -60,48 +74,59 @@ while True:
             "Incorrect reply from Webex Teams API. Status code: {}".format(r.status_code)
         )
 
-    # get the JSON formatted returned data
     json_data = r.json()
-
-    # check if there are any messages in the "items" array
     if len(json_data["items"]) == 0:
         raise Exception("There are no messages in the room.")
-
-    # store the array of messages
     messages = json_data["items"]
-    
-    # store the text of the first message in the array
     message = messages[0]["text"]
-    print("Received message: " + message)
+    if not message.startswith(f"/{student_id} "):
+        continue
+    print("My message: " + message)
+    parts = message.split()
+    if len(parts) < 2:
+        continue
+    tokens = parts[1:]
+    responseMessage = ""
 
-    # check if the text of the message starts with the magic character "/" followed by your studentID and a space and followed by a command name
-    #  e.g.  "/66070123 create"
-    if message.startswith(f"/{student_id} "):
-
-        # extract the command
-        command = message.split(" ", 1)[1].strip()
-        #print(command)
-        my_student_id = message.split(" ", 1)[0][1:]
-        #print(my_student_id)
-
-# 5. Complete the logic for each command
-
-        if command == "create":
-            responseMessage = create(my_student_id)
-        elif command == "delete":
-            responseMessage = delete(my_student_id)
-        elif command == "enable":
-            responseMessage = enable(my_student_id)
-        elif command == "disable":
-            responseMessage = disable(my_student_id)
-        elif command == "status":
-            responseMessage = status(my_student_id)
-        elif command == "gigabit_status":
-            responseMessage = gigabit_status()
-        elif command == "showrun":
-            responseMessage = showrun()
+    if len(tokens) == 1:
+        # case "/66070006 restconf" or "/66070006 create"
+        text = tokens[0]
+        if text in ["restconf", "netconf"]:
+            responseMessage = check_type_command(text)
+        elif text.count(".") == 3:  # check IP
+            responseMessage = "Error: No command found."
+        elif command_type == 0 and text in ["create", "delete", "enable", "disable", "status"]:
+            responseMessage = "Error: No method specified"
         else:
-            responseMessage = "Error: No command or unknown command"
+            responseMessage = "Error: No IP specified"
+
+    elif len(tokens) == 2:
+        # case"/66070006 10.0.15.61 create"
+        router_ip = tokens[0]
+        command = tokens[1]
+
+        if command_type == 0:
+            responseMessage = "Error: No method specified"
+        elif not router_ip:
+            responseMessage = "Error: No IP specified"
+        else:
+            if command_type == 1:
+                module = restconf_final
+            elif command_type == 2:
+                module = netconf_final
+
+            if command == "create":
+                responseMessage = module.create(student_id, router_ip)
+            elif command == "delete":
+                responseMessage = module.delete(student_id, router_ip)
+            elif command == "enable":
+                responseMessage = module.enable(student_id, router_ip)
+            elif command == "disable":
+                responseMessage = module.disable(student_id, router_ip)
+            elif command == "status":
+                responseMessage = module.status(student_id, router_ip)
+            else:
+                responseMessage = "Error: Unknown command"
         
 # 6. Complete the code to post the message to the Webex Teams room.
 
@@ -117,30 +142,30 @@ while True:
         # Read Send a Message with Attachments Local File Attachments
         # https://developer.webex.com/docs/basics for more detail
 
-        if command == "showrun" and responseMessage == 'ok':
-                files = glob.glob("./show_run_66070006*")
-                latest_file = max(files, key=os.path.getmtime)
-                fileobject = open(latest_file, "rb")
-                filetype = "text/plain"
-                postData = MultipartEncoder(
-                    fields={
-                        "roomId": roomIdToGetMessages,
-                        "text": "show running config",
-                        "files": (os.path.basename(latest_file), fileobject, filetype)
-                        })
-                HTTPHeaders = {
-                    "Authorization": f"Bearer {ACCESS_TOKEN}",
-                    "Content-Type": postData.content_type,}
-        else:
-            postData = {"roomId": roomIdToGetMessages, "text": responseMessage}
-            postData = json.dumps(postData)
-            HTTPHeaders = {
+        # if command == "showrun" and responseMessage == 'ok':
+        #         files = glob.glob("./show_run_66070006*")
+        #         latest_file = max(files, key=os.path.getmtime)
+        #         fileobject = open(latest_file, "rb")
+        #         filetype = "text/plain"
+        #         postData = MultipartEncoder(
+        #             fields={
+        #                 "roomId": roomIdToGetMessages,
+        #                 "text": "show running config",
+        #                 "files": (os.path.basename(latest_file), fileobject, filetype)
+        #                 })
+        #         HTTPHeaders = {
+        #             "Authorization": f"Bearer {ACCESS_TOKEN}",
+        #             "Content-Type": postData.content_type,}
+        # else:
+    postData = {"roomId": roomIdToGetMessages, "text": responseMessage}
+    postData = json.dumps(postData)
+    HTTPHeaders = {
             "Authorization": f"Bearer {ACCESS_TOKEN}",
             "Content-Type": "application/yang-data+json"
-            }   
+        }   
 
-        r = requests.post("https://webexapis.com/v1/messages", data=postData, headers=HTTPHeaders)
-        if not r.status_code == 200:
-            raise Exception(
+    r = requests.post("https://webexapis.com/v1/messages", data=postData, headers=HTTPHeaders)
+    if not r.status_code == 200:
+        raise Exception(
                 "Incorrect reply from Webex Teams API. Status code: {}".format(r.status_code)
             )
